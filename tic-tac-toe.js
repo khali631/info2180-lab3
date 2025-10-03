99c88b2d1a892b61d7e6cf49c7a9be6ef3e350ed
khali631 <khaleciajstephenson@gmail.com>
2025-10-03 00:55:17 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,14 @@
+const checkWin = (grid) => {
+  const size = 3; // 3x3 grid
+  let winner = null;
 
+  // Check all rows
+  for (let row = 0; row < size; row++) {
+    if (
+      grid[row][0] !== "" &&
+      grid[row][0] === grid[row][1] &&
+      grid[row][1] === grid[row][2]
+    ) {
+      winner = grid[row][0]; // Returns the winner ("X" or "O")
+    }
+  }
